5715e4cc36df4ac749e479701ce54d026703dacc
tillenotuoma <totuoma27@gmail.com>
2025-03-06 14:04:19 +0300

    done lab

---
+++ app/migration/versions/40c11d197902_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 40c11d197902
+Revises: 
+Create Date: 2025-03-06 12:30:43.706448
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '40c11d197902'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
